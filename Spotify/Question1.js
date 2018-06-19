6517b7a174d0509b1f59621e6825001a9fcddd0c
Eitan Fine <33404825+EitanFine@users.noreply.github.com>
2018-06-19 09:59:23 -0400

    Final Commit for Spotify

---
+++ Spotify/Question1.js
@@ -0,0 +1,23 @@
+
+// Question 1 -- sortByStrings(s,t):
+// Sort the letters in the string s by the order they occur in the string t. 
+// You can assume t will not have repetitive characters. 
+
+function sortByStrings(s, t) {
+    return s.split("").sort((a, b) => {
+        if (t.indexOf(a) < t.indexOf(b)) {
+            return -1
+        }
+        if (t.indexOf(a) > t.indexOf(b)) {
+            return 1
+        }
+        return 0
+    }).join("")
+
+}
+
+// For s = "weather" and t = "therapyw", 
+//     the output should be sortByString(s, t) 
+//     = "theeraw". For s = "good" and t = "odg", the output should be sortByString(s, t) = "oodg".
+let answer = sortByStrings("weather", "therapyw")
+console.log(answer)
